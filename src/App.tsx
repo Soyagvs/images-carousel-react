115bd7d19a7668640a0a45283122f074780a7351
Soyagvs <agustinmartinezyt@gmail.com>
2024-05-20 23:38:28 -0300

    create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Carousel from './components/Carousel'
+import image1 from './assets/img/img1.jpg'
+import image2 from './assets/img/img2.jpg'
+import image3 from './assets/img/img3.jpg'
+import image4 from './assets/img/img4.jpg'
+import image5 from './assets/img/img5.jpg'
+import image6 from './assets/img/img6.jpg'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const imgs = [image1, image2, image3, image4, image5, image6]
+
+const App = () => {
+  return <Carousel slides={imgs} />
 }
 
 export default App
